e6dfc6aa86681cf774d70daec2e3896612f16b2b
Lucas Vida <lucasvida@gmail.com>
2024-11-24 20:58:07 -0300

    Imersão Dev Beck-End | Alura | Aula 4

--- src/models/postsModel.js
+++ src/models/postsModel.js
@@ -2,10 +2,14 @@ import conectarAoBanco from "../config/dbConfig.js";
 
 const conexao = await conectarAoBanco(process.env.STRING_CONEXAO);
 
-async function getTodosPosts() {
+export async function getTodosPosts() {
   const db = conexao.db("imersao-instabyte");
   const colecao = db.collection("posts");
   return colecao.find().toArray();
 }
 
-export default getTodosPosts;
+export async function criarPost(novoPost) {
+  const db = conexao.db("imersao-instabyte");
+  const colecao = db.collection("posts");
+  return colecao.insertOne(novoPost);
+}
